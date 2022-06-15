2a47603878c18882891a5ed5586638e05f5cf842
Nate Hamshar <williamhamshar@gmail.com>
2022-06-15 09:07:45 -0400

    added animations and text overlays on images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import './App.css';
 function App() {
+
+
+
+  const reveal = () => {
+    let reveals = document.querySelectorAll(".reveal")
+
+    for (let i = 0; i < reveals.length; i++){
+      let windowheight = window.innerHeight;
+      let revealtop = reveals[i].getBoundingClientRect().top;
+      let revealpoint = 150;
+
+      if (revealtop < windowheight - revealpoint){
+        reveals[i].classList.add('active')
+      } else {
+        reveals[i].classList.remove('active')
+      }
+    }
+  }
+  window.addEventListener('scroll', reveal);
+
   return (
     <div className="App">
       <div className="landing">
@@ -7,27 +27,45 @@ function App() {
         <h1 className='nameSecond'>A'glazing</h1>
         <p className='slogan'>Donuts done right<span className="period">.</span></p>
       </div>
+
       <div className="border"></div>
-      <div className="contentRight">
+
+      <div className="contentRight reveal">
         <div className="imgRight">
           <img src={require("./images/blueLemon.jpg")} alt="donuts" className='images' />
         </div>
-        <h1 className="rightText">Delicious</h1>
+        <div className="overlayRight">
+          <h1>You've Got Options</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum soluta quibusdam necessitatibus similique nam.</p>
+          <a href="/" className="overlayLink">Look at our menu!</a>
+        </div>
       </div>
-      <div className="contentLeft">
+
+      <div className="contentLeft reveal">
         <div className="imgLeft">
           <img src={require("./images/germanChoc.jpg")} alt="donuts" className='images' />
         </div>
-        <h1 className="leftText">Homemade</h1>
+        <div className="overlayLeft">
+          <h1>We make ordering easy</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti sed est eos.</p>
+          <a href="/" className="overlayLink">Order Now!</a>
+        </div>
       </div>
-      <div className="contentRight">
+
+      <div className="contentRight reveal">
         <div className="imgRight">
           <img src={require("./images/cookieMonster.jpg")} alt="donuts" className='images' />
         </div>
-        <h1 className="rightText">Perfection</h1>
+        <div className="overlayRight">
+          <h1>Yes, We Cater</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit praesentium architecto, aperiam illo vitae magnam!</p>
+          <a href="/" className="overlayLink">Got to Catering</a>
+        </div>
       </div>
     </div>
   );
+
+  
 }
 
 export default App;
